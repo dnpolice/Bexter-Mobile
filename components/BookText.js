a4c92cf3c101f533c59b7b37033a5c814b77a1c8
Dylan Policelli <dylanpolicelli@gmail.com>
2023-02-19 18:46:10 -0500

    add favourite icon

--- components/BookText.js
+++ components/BookText.js
@@ -1,4 +1,5 @@
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Pressable} from 'react-native';
+import FavouriteIcon from './FavouriteIcon';
 
 const BookText = ({title, author, keyLearningOutcomes}) => {
     const keyLearningOutcomesString = keyLearningOutcomes.reduce((t, c, i) => {
@@ -8,11 +9,14 @@ const BookText = ({title, author, keyLearningOutcomes}) => {
 
     return (
         <View style={styles.bookText}>
-            <View>
+            <Pressable style={styles.favouriteIcon}>
+                <FavouriteIcon />
+            </Pressable>
+            <View style={styles.textGroup}>
                 <Text style={styles.text}>{title}</Text>
                 <Text style={styles.text}>by {author}</Text>
             </View>
-            <View>
+            <View style={styles.textGroup}>
                 <Text style={styles.text}>Learning Outcomes</Text>
                 <Text style={{...styles.keywords, ...styles.text}}>{keyLearningOutcomesString}</Text>
             </View>
@@ -25,7 +29,7 @@ const styles = StyleSheet.create({
     bookText: {
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'space-evenly',
+        justifyContent: 'space-between',
         height: '100%',
         alignItems: 'start',
         width: 150,
@@ -37,6 +41,14 @@ const styles = StyleSheet.create({
     text: {
         fontSize: 16,
         paddingVertical: 2
+    },
+    favouriteIcon: {
+        position: 'absolute',
+        top: 5,
+        right: 0,
+    },
+    textGroup: {
+        paddingVertical: 10,
     }
 });
 
---
+++ components/FavouriteIcon.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Svg, { Path } from "react-native-svg";
+const SVGComponent = (props) => (
+  <Svg
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <Path
+      d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z"
+      stroke="#25282B"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </Svg>
+);
+export default SVGComponent;
