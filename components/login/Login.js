69b5faaccfb70fab7d669c2feceee5d66e22b0af
unknown <hanheidibms@gmail.com>
2023-03-19 00:55:30 -0400

    move api call to login container

--- components/login/Login.js
+++ components/login/Login.js
@@ -1,63 +1,17 @@
-import {StyleSheet, Text, View, Alert } from 'react-native';
+import {StyleSheet, Text, View } from 'react-native';
 import LoginButtons from './LoginButtons';
 import LoginInput from './LoginInput';
 import {useState} from 'react';
 
-const base_url = "http://3.134.99.13:5000/";
-// const base_url = "http://localhost:5000/";
-const loginUrl = base_url + 'auth/login';
-
-const Login = ({navigation}) => {
+const Login = ({navigation,loginUser}) => {
   const [isLoginPressed, setIsLoginPressed] = useState(false);
 
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
 
   if (isLoginPressed) {
-    const loginUser = async () => {
-      const login = () => {
-        return fetch(loginUrl, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          }, 
-          body:JSON.stringify({
-            "email":userEmail, 
-            "password": userPassword
-          }),
-          
-        })
-      } ;
-      const loginResponse = login();
-      loginResponse.then((response) => {
-        const cookie =  response.headers.map["set-cookie"];
-        setIsLoginPressed(false);
-        if (response.ok === true) {
-          // get cookie and name somehow and pass in to home
-          // these need to be persisted 
-          // using userEmail just for now
-          navigation.navigate('Home', {
-            userName: "Tim"
-          });
-        }
-        else {
-          Alert.alert('Login Error', 'Login credentials invalid', [
-            {
-              text: 'OK',
-            }
-          ]);
-          // then maybe clear the input fields
-        }
-      }).catch(e => {
-        setIsLoginPressed(false);
-        Alert.alert('Login Error', 'Server error', [
-          {
-            text: 'OK',
-          }
-        ]);
-      });
-    }
-    loginUser()
+    loginUser(userEmail, userPassword);
+    setIsLoginPressed(false);
   }
   return (
     <View style={styles.login}>
--- components/login/LoginContainer.js
+++ components/login/LoginContainer.js
@@ -1,14 +1,62 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Alert } from 'react-native';
 import Login from './Login';
 import BackgroundOverlay from '../general/BackgroundOverlay';
 import LogoContainer from '../general/LogoContainer';
 
+const base_url = "http://3.134.99.13:5000/";
+// const base_url = "http://localhost:5000/";
+const loginUrl = base_url + 'auth/login';
+
 const LoginContainer = ({navigation}) => {
+  const loginUser = async (userEmail, userPassword) => {
+    const login = () => {
+      return fetch(loginUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        }, 
+        body:JSON.stringify({
+          "email":userEmail, 
+          "password": userPassword
+        }),
+        
+      })
+    } ;
+    const loginResponse = login();
+    loginResponse.then((response) => {
+      const cookie =  response.headers.map["set-cookie"];
+      if (response.ok === true) {
+        // get cookie and name somehow and pass in to home
+        // these need to be persisted 
+        // using userEmail just for now
+        navigation.navigate('Home', {
+          userName: "Tim"
+        });
+      }
+      else {
+        Alert.alert('Login Error', 'Login credentials invalid', [
+          {
+            text: 'OK',
+          }
+        ]);
+        // then maybe clear the input fields
+      }
+    }).catch(e => {
+      Alert.alert('Login Error', 'Server error', [
+        {
+          text: 'OK',
+        }
+      ]);
+    });
+   
+  }
   return (
     <View style={styles.loginContainer}>
         <BackgroundOverlay />
         <LogoContainer />
-        <Login navigation={navigation}/>
+        <Login 
+        navigation={navigation}
+        loginUser={loginUser}/>
     </View>
   );
 }
